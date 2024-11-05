0ea5fbb2006fafdfe7e44f81d0c7b9442ac998de
Admin <Admin@DESKTOP-CFB93T8>
2024-11-05 17:10:33 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentSync
+{
+    internal class Program
+    {
+        enum Menu
+        {
+            
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
